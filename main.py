aac86107f583727a2b57bcb993260829dddb0ad7
Wazz <Wazzduh@gmail.com>
2025-08-12 17:56:23 -0700

    added player and player turning

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2), PLAYER_RADIUS)
 
     while True:
 
@@ -12,7 +16,10 @@ def main():
                 return
 
         pygame.Surface.fill(screen, (0,0,0))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = (clock.tick(60)) / 1000
 
 
 if __name__ == "__main__":
